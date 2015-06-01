ad75dc869441160c8276d46f82909a5a4d9abf9a
Axure <axurez@vip.qq.com>
2015-06-01 19:11:20 +0800

    Initial Commit

---
+++ Hexium-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hexium-Swift
+//
+//  Created by 郑虎 on 15 年 6. 1..
+//  Copyright (c) 2015年 郑虎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Hexium-SwiftTests/Hexium_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Hexium_SwiftTests.swift
+//  Hexium-SwiftTests
+//
+//  Created by 郑虎 on 15 年 6. 1..
+//  Copyright (c) 2015年 郑虎. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Hexium_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
